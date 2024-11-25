95726c354dd0a226fa60265b527af596e1e6d9ec
Anisia9 <grishkoanisia@gmail.com>
2024-11-25 21:19:56 +0300

    Add files via upload

---
+++ google_sheets.py
@@ -0,0 +1,14 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def create_sheet_connection(json_file, sheet_name):
+    """Подключаем гугл таблицу"""
+    scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"] #права доступа
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file, scopes) #данные доступа
+    gc = gspread.authorize(credentials) #авторизация
+    spreadsheet = gc.open(sheet_name) #открываем таблицу
+    return spreadsheet.sheet1 #указание на то что мы работаем на 1 листе таблице
+
+def add_to_sheet(sheet, row):
+  """Добавляет данные в таблицу"""
+  sheet.append_row(row) #создание новой строки
